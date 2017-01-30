90a99e145be0c4e858965fa7e8ca8ddf5f4a9ba6
Casey Leask <caseyleask@gmail.com>
2017-01-30 23:01:59 +1100

    Create output directories if they don't exist

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,6 +1,13 @@
+require 'uri'
+
 class Runner
   attr_accessor :api_url, :output_directory
   def initialize(argv)
     raise ArgumentError unless argv.length == 2
+    @api_url = URI(argv[0])
+    raise URI::InvalidURIError if @api_url.relative?
+
+    Dir.mkdir(argv[1]) unless Dir.exist?(argv[1])
+    @output_directory = Dir.new(argv[1])
   end
 end
--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -1,3 +1,4 @@
+require 'uri'
 require_relative '../lib/runner'
 
 describe Runner do
@@ -25,8 +26,73 @@ describe Runner do
       end
     end
 
-    context 'with a valid api_url and output_directory' do
+    context 'with api_url and output_directory' do
       let(:argv) { [ api_url, output_directory ] }
+      let(:api_url) { 'https://example.com/api' }
+      let(:output_directory) { '.' }
+
+      context 'given an invalid URL for api_url' do
+        let(:api_url) { '^' }
+
+        it 'should raise an error' do
+          expect { runner }.to raise_error(URI::InvalidURIError)
+        end
+      end
+
+      context 'given a relative URL for api_url' do
+        let(:api_url) { 'api' }
+
+        it 'should raise an error' do
+          expect { runner }.to raise_error(URI::InvalidURIError)
+        end
+      end
+
+      context 'given an absolute URL for api_url' do
+        it 'should be accessible' do
+          expect(runner.api_url.to_s).to eq(api_url)
+        end
+      end
+
+      context 'given an invalid directory for output_directory' do
+        let(:output_directory) { '' }
+        it 'should raise an error' do
+          expect { runner }.to raise_error(Errno::ENOENT)
+        end
+      end
+
+      context 'given a non-existent directory for output_directory' do
+        let(:output_directory) { 'non-existent' }
+
+        around(:each) do |test|
+          Dir.rmdir(output_directory) if Dir.exist?(output_directory)
+          test.run
+          Dir.rmdir(output_directory)
+        end
+
+        it 'should create the directory' do
+          runner
+
+          expect(Dir.exist?(output_directory)).to be true
+        end
+
+        it 'should be accessible' do
+          expect(runner.output_directory.path).to eq(output_directory)
+        end
+      end
+
+      context 'given an existing directory for output_directory' do
+        let(:output_directory) { 'existing' }
+
+        around(:each) do |test|
+          Dir.mkdir(output_directory)
+          test.run
+          Dir.rmdir(output_directory)
+        end
+
+        it 'should be accessible' do
+          expect(runner.output_directory.path).to eq(output_directory)
+        end
+      end
     end
   end
 end
