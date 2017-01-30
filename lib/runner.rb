2a47652159de4f7838d9a260c9ac61be15ebe6cd
Casey Leask <caseyleask@gmail.com>
2017-01-30 23:10:19 +1100

    Use URI.parse over URI()

--- lib/runner.rb
+++ lib/runner.rb
@@ -4,7 +4,7 @@ class Runner
   attr_accessor :api_url, :output_directory
   def initialize(argv)
     raise ArgumentError unless argv.length == 2
-    @api_url = URI(argv[0])
+    @api_url = URI.parse(argv[0])
     raise URI::InvalidURIError if @api_url.relative?
 
     Dir.mkdir(argv[1]) unless Dir.exist?(argv[1])
