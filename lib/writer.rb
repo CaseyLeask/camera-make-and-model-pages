9cc4bd12ccc0f21c40dfa27a683bba5c19603044
Casey Leask <caseyleask@gmail.com>
2017-02-01 00:05:10 +1100

    Remove commented out testing code

--- lib/writer.rb
+++ lib/writer.rb
@@ -8,7 +8,6 @@ require_relative 'model_pages'
 # Used to write the pages to the output directory
 class Writer
   def self.write(pages, output_directory)
-    # pages.each { |_k, page| puts page }
     pages.each do |filename, page|
       path = File.expand_path(filename, output_directory)
       File.write(path, page)
